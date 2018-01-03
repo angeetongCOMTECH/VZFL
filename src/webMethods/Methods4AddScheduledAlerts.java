2cafa5557635dbae2a76ebb19ed80f7d5e7df30c
Angela Tong <angela.tong@comtechtel.com>
2018-01-02 20:33:26 -0800

    Add windows chromedriver and geckodriver so windows user can run tests; Also added scheduled alerts methods

---
+++ src/webMethods/Methods4AddScheduledAlerts.java
@@ -0,0 +1,25 @@
+package webMethods;
+
+import java.io.IOException;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.testng.asserts.SoftAssert;
+
+public class Methods4AddScheduledAlerts {
+	
+	//****************************************//
+	//***                                  ***//
+	//*** Created by Angela Tong Dec 2017  ***//
+	//***                                  ***//
+	//****************************************//
+
+		static SoftAssert softAssert = new SoftAssert();
+		final static Logger log = Logger.getLogger(Methods4AddScheduledAlerts.class);
+		
+		public static void addScheduledAlert (WebDriver webdriver, String foldername, String errorname, String error, String contactname, String phone, String email) throws InterruptedException, IOException
+		{
+			
+		
+		}
+
+}
